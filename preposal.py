12389c5e843fcdc2ac0c735b1f05d3820c29fe48
megoldd <102323196+megoldd@users.noreply.github.com>
2022-03-29 14:49:17 -0400

    Create preposal.py

---
+++ preposal.py
@@ -0,0 +1,15 @@
+# Preproposal
+
+## What idea(s) do you have for your final project?
+
+create snake game
+determine future stock prices
+
+
+## If you plan to collaborate with one or two classmates, what are their names?
+
+TODO
+
+## Do you have any questions of your own?
+
+TODO
